2d8a8b54b13054a5de5d51e8ab3a3a0f307cc90d
DC Wang <livings5858@163.com>
2024-08-24 19:28:29 +0800

    模拟相机和物理相机OK

--- RealCameraDeal.py
+++ RealCameraDeal.py
@@ -1,12 +1,25 @@
 import paho.mqtt.client as mqtt
+from fireDetection import detect_fire
 
 BROKER = "localhost"
 TOPIC = "image_data"
 DONE_TOPIC = "image_send_done"
 CHUNK_SIZE = 64
 
+BASE_PATH = "D:\\code\\weihai\\code\\springboot-vue\\ui\\public\\"
+imglist = ["nofire2.png", "fire2.png"]
+index = 1
+
+def publish_image_msg(client, fileName):
+    msg_image_info = client.publish("image_path", fileName, qos=1)
+    imageFullPath = BASE_PATH + fileName
+    print (imageFullPath)
+    if detect_fire(imageFullPath):
+        msg_image_detect_info = client.publish("image_dectected", "fire", qos=1)
+
 image_data = {}
 def on_message(client, userdata, msg):
+    global index
     if msg.topic == TOPIC:
         # 接收数据并解析分段ID
         chunk_id = int(msg.payload[:4])
@@ -15,23 +28,24 @@ def on_message(client, userdata, msg):
         # 存储图像数据
         image_data[chunk_id] = data
     elif msg.topic == DONE_TOPIC:
-        with open("received_image.jpg", "wb") as file:
+        imgName = f"received_image_{index}.jpg"
+        with open(BASE_PATH + imgName, "wb") as file:
             for i in sorted(image_data.keys()):
                 file.write(image_data[i])
-        print("Image received and saved as 'received_image.jpg'")
+        print("Image received and saved as " + imgName)
         image_data.clear()
+        publish_image_msg(client, imgName)
+        index += 1
 
-# The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
     client.subscribe(TOPIC)
     client.subscribe(DONE_TOPIC)
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.on_connect = on_connect
 client.on_message = on_message
+
 
 client.connect(BROKER, 1883, 60)
 client.loop_forever()
