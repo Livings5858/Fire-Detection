9ef7fc2e905f93543711c67361621394b08c0b53
DC Wang <livings5858@163.com>
2024-08-24 11:21:55 +0800

    只模拟图像数据，温度和位置由开发板提供

--- mqttPublisher.py
+++ mqttPublisher.py
@@ -27,19 +27,23 @@ mqttc.on_publish = on_publish
 mqttc.user_data_set(unacked_publish)
 mqttc.connect("localhost")
 
-BASE_PATH = "D:\\code\\linux\\embedded\\teach\\springboot-vue\\ui\\vue-project\\public\\"
+BASE_PATH = "D:\\code\\weihai\\code\\springboot-vue\\ui\\public\\"
 
 imglist = ["nofire2.png", "fire2.png"]
 index = 0
 while True:
-    temperature = round(random.uniform(10, 50))
-    x = round(random.uniform(10, 50))
-    y = round(random.uniform(10, 50))
-    msg = f"{temperature},{x},{y}"
     mqttc.loop_start()
-    msg_sensor_info = mqttc.publish("sensor", msg, qos=1)
-    unacked_publish.add(msg_sensor_info.mid)
 
+    # sensor
+    # temperature = round(random.uniform(10, 50))
+    # x = round(random.uniform(10, 50))
+    # y = round(random.uniform(10, 50))
+    # msg = f"{temperature},{x},{y}"
+    # msg_sensor_info = mqttc.publish("sensor", msg, qos=1)
+    # unacked_publish.add(msg_sensor_info.mid)
+    # msg_sensor_info.wait_for_publish()
+
+    # image
     msg_image_info = mqttc.publish("image_path", imglist[index], qos=1)
     unacked_publish.add(msg_image_info.mid)
     imageFullPath = BASE_PATH + imglist[index]
@@ -51,8 +55,8 @@ while True:
     index += 1
     if index >= len(imglist):
         index = 0
-    msg_sensor_info.wait_for_publish()
     msg_image_info.wait_for_publish()
+
     mqttc.loop_stop()
     time.sleep(5)
 
